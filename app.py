315bbba4587c094321fd20ce20379ad8ac3326aa
EndrianoKitswanto-231401073 <endriano28@gmail.com>
2026-05-26 23:45:45 +0700

    fix : fix test app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes.smart_spending_insight_routes import smart_spending_insight_bp
 from routes.ai_financial_assistant_routes import ai_financial_assistant_bp
 from routes.smart_investment_advisor_routes import smart_investment_advisor_bp
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.register_blueprint(smart_spending_insight_bp)
 app.register_blueprint(ai_financial_assistant_bp)
